6253b0ade4b5f1a40e6f452265ec3a0e702f85a4
Dmytro <motryk@dlit.dp.ua>
2018-12-16 16:11:09 +0200

    init project

---
+++ DebtsAPI/Controllers/DebtsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebtsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DebtsController : ControllerBase
+    {
+        // GET api/debts
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
