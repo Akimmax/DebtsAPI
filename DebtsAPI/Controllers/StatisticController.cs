52454115ed9c9a93ec3c5a4aecbc8d82f86753cf
Максим Акименко <Akymenko.M.O@nmu.one>
2019-01-06 00:33:40 +0200

    Enable get total statistic

---
+++ DebtsAPI/Controllers/StatisticController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using DebtsAPI.Services;
+
+namespace DebtsAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(401)]
+    public class StatisticController : ControllerBase
+    {
+        private readonly IStatisticService _statisticService;
+
+        public StatisticController(IStatisticService statisticService)
+        {
+            _statisticService = statisticService;
+        }
+
+        [HttpGet]
+        public IActionResult GetTotalStatistic()
+        {
+            return Ok(_statisticService.GetTotalStatistic());
+        }
+
+    }
+}
---
+++ DebtsAPI/Services/StatisticService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using DebtsAPI.Data;
+using DebtsAPI.Dtos;
+
+namespace DebtsAPI.Services
+{
+    public interface IStatisticService
+    {
+        StatisticDto GetTotalStatistic();
+    }
+
+    public class StatisticService : IStatisticService
+    {
+        private readonly DatabaseContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public StatisticService(DatabaseContext context, IHttpContextAccessor httpContextAccessor)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public StatisticDto GetTotalStatistic()
+        {
+            var userId = Convert.ToInt32(_httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            StatisticDto statistic = new StatisticDto()
+            {
+                Gave = _context.Debts.Where(debt => debt.GiverId == userId).Sum(n => n.Sum),
+                Took = _context.Debts.Where(debt => debt.TakerId == userId).Sum(n => n.Sum)
+            };
+
+            return statistic;
+        }
+    }
+}
