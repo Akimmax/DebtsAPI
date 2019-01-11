39da1a1d03cc626c9bf3b055c2adbe3c52483ce5
Максим Акименко <Akymenko.M.O@nmu.one>
2019-01-11 17:29:49 +0200

    Added migrations

---
+++ DebtsAPI/Migrations/20190110223422_AddPaymentsMigration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DebtsAPI.Migrations
+{
+    public partial class AddPaymentsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "Debts",
+                newName: "isRepaid");
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DebtId = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Amount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Debts_DebtId",
+                        column: x => x.DebtId,
+                        principalTable: "Debts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_DebtId",
+                table: "Payments",
+                column: "DebtId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.RenameColumn(
+                name: "isRepaid",
+                table: "Debts",
+                newName: "IsActive");
+        }
+    }
+}
